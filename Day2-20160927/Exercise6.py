c794e9b92c581de098b42fdb527e59d01de47df0
dimoskag <dimoskag@users.noreply.github.com>
2016-10-02 22:36:30 +0300

    Delete Exercise6.py

--- Day2-20160927/Exercise6.py
+++
@@ -1,5 +0,0 @@
-triNumber = int(input("Please, enter the number of triangular numbers you want to produce:"))
-for n in range (1,triNumber+1):
-    amount = (n*(n + 1))//2
-    print (amount, end=' ')
-    
